7d041f60c191286301f2e4be7509fe9715ec51e2
wallace <wallacegibbon@outlook.com>
2020-05-11 10:25:36 +0800

    jsonerl encode/decode is moved out

--- src/serval_api.erl
+++ src/serval_api.erl
@@ -2,9 +2,6 @@
 
 -export([init/2, terminate/3]).
 
--include_lib("eunit/include/eunit.hrl").
-
-
 %%----------------------------------------------------------------------
 %% Http related things
 %%----------------------------------------------------------------------
@@ -28,7 +25,7 @@ extract_atom(Binary) ->
 
 decode_apiargument(Rawbody) ->
     try
-	check_params(decode(fix_emptybody(Rawbody)))
+	check_params(jsonerl:decode(fix_emptybody(Rawbody)))
     catch
 	throw:{invalid_json, _V} ->
 	    throw({serval, "argument is not valid"})
@@ -76,7 +73,7 @@ api(Module, Function, Arguments) ->
 handle_request(jsonerl, Module, Fn, Argument) ->
     R = api(Module, Fn, decode_apiargument(Argument)),
     RespHeaders = #{<<"content-type">> => <<"application/json">>},
-    {RespHeaders, encode(R)};
+    {RespHeaders, jsonerl:encode(R)};
 handle_request(raw, Module, Fn, Argument) ->
     R = api(Module, Fn, [Argument]),
     {#{}, ensure_binary(R)};
@@ -91,7 +88,7 @@ handle_common(Req0) ->
 	{Req2, RespHeaders, Body}
     catch
 	throw:{serval, Info}->
-	    {Req0, #{}, encode({error, ensure_binary(Info)})}
+	    {Req0, #{}, jsonerl:encode({error, ensure_binary(Info)})}
     end.
 
 %% stack traces for unknown error are printed
@@ -101,7 +98,7 @@ safe_handle(Req0) ->
     catch
 	T:I:S ->
 	    io:format("*~p: ~p, stacktrace:~n~p~n", [T, I, S]),
-	    {Req0, #{}, encode({fatal, <<"server error">>})}
+	    {Req0, #{}, jsonerl:encode({fatal, <<"server error">>})}
     end.
 
 init(Req0, State) ->
@@ -115,97 +112,6 @@ terminate(_Reason, _Req, _State) ->
 %%----------------------------------------------------------------------
 %% term encoding and decoding
 %%----------------------------------------------------------------------
-
-encode(Anything) ->
-    try
-	jsone:encode(encode_1(Anything))
-    catch
-	error:badarg ->
-	    throw({invalid_json, Anything})
-    end.
-
-encode_1(E) when is_atom(E) ->
-    #{<<"t">> => <<"atom">>, <<"v">> => list_to_binary(atom_to_list(E))};
-encode_1(E) when is_tuple(E) ->
-    #{<<"t">> => <<"tuple">>, <<"v">> => encode_1(tuple_to_list(E))};
-encode_1(E) when is_map(E) ->
-    #{<<"t">> => <<"map">>, <<"v">> => encode_1(maps:to_list(E))};
-encode_1(E) when is_binary(E) ->
-    ensure_utf8(E);
-encode_1(E) when is_number(E) ->
-    E;
-encode_1([E | Rest]) ->
-    [encode_1(E) | encode_1(Rest)];
-encode_1([]) ->
-    [];
-encode_1(V) ->
-    throw({invalid_json, V}).
-
-%% erlang binary could be NOT utf8
-%% @TODO: use a better way to check valid utf8 string
-ensure_utf8(Binary) when is_binary(Binary) ->
-    try
-	jsone:encode(Binary),
-	Binary
-    catch
-	error:badarg ->
-	    <<"non-utf8">>
-    end.
-
-
-decode(Anything) ->
-    try
-	decode_1(jsone:decode(Anything))
-    catch
-	error:badarg ->
-	    throw({invalid_json, Anything})
-    end.
-
-decode_1(#{<<"t">> := <<"atom">>, <<"v">> := V}) ->
-    binary_to_existing_atom(V, utf8);
-decode_1(#{<<"t">> := <<"tuple">>, <<"v">> := V}) ->
-    list_to_tuple(decode_1(V));
-decode_1(#{<<"t">> := <<"map">>, <<"v">> := V}) ->
-    maps:from_list(decode_1(V));
-decode_1(E) when is_binary(E); is_number(E) ->
-    E;
-decode_1([E | Rest]) ->
-    [decode_1(E) | decode_1(Rest)];
-decode_1([]) ->
-    [];
-decode_1(V) ->
-    throw({invalid_json, V}).
-
--ifdef(TEST).
-
-encode_decode_maps() ->
-    [{1, <<"1">>},
-     {a, <<"{\"t\":\"atom\",\"v\":\"a\"}">>},
-     {<<"ab">>, <<"\"ab\"">>},
-     {{1,2}, <<"{\"t\":\"tuple\",\"v\":[1,2]}">>},
-     {[1,2], <<"[1,2]">>},
-     {[1,[2,[3]]], <<"[1,[2,[3]]]">>},
-     {[1,[2,[<<"blah">>]]], <<"[1,[2,[\"blah\"]]]">>},
-     {#{}, <<"{\"t\":\"map\",\"v\":[]}">>},
-     {#{a => 1, b => 2}, <<"{\"t\":\"map\",\"v\":[{\"t\":\"tuple\",\"v\":[{\"t\":\"atom\",\"v\":\"a\"},1]},{\"t\":\"tuple\",\"v\":[{\"t\":\"atom\",\"v\":\"b\"},2]}]}">>},
-     {#{a => #{b => 2}}, <<"{\"t\":\"map\",\"v\":[{\"t\":\"tuple\",\"v\":[{\"t\":\"atom\",\"v\":\"a\"},{\"t\":\"map\",\"v\":[{\"t\":\"tuple\",\"v\":[{\"t\":\"atom\",\"v\":\"b\"},2]}]}]}]}">>}
-    ].
-
-encode_test_() ->
-    lists:map(fun({V, T}) ->
-		      %?debugFmt("~p~n", [encode(V)]),
-		      ?_assert(encode(V) =:= T)
-	      end, encode_decode_maps()).
-
-encode_invliad_utf8_test() ->
-    ?assert(encode(<<192>>) =:= <<"\"non-utf8\"">>).
-
-decode_test_() ->
-    lists:map(fun({V, T}) ->
-		      ?_assert(decode(T) =:= V)
-	      end, encode_decode_maps()).
-
--endif.
 
 %%----------------------------------------------------------------------
 %% utilities
